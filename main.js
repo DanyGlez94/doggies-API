090044d68fd04d8a35fb91d5c18712f8b6164168
DanyGlez94 <daniel_glez94@hotmail.com>
2022-04-30 18:42:50 -0500

    Añadido el query limit para que muestre los datos de 3 perros por cada consulta, también añadidas las secciones al HTML

--- main.js
+++ main.js
@@ -1,47 +1,55 @@
-const URL = "https://api.thedogapi.com/v1/images/search";
-const img = document.querySelector("img");
-const title = document.getElementById("title");
-const weight = document.getElementById("weight");
-const height = document.getElementById("height");
-const lifeSpan = document.getElementById("lifeSpan");
+const API_URL = "https://api.thedogapi.com/v1/images/search?limit=3";
+const images = document.getElementsByTagName("img");
+const titles = document.getElementsByClassName("card__title");
+const weights = document.getElementsByClassName("weight");
+const heights = document.getElementsByClassName("height");
+const lifeSpan = document.getElementsByClassName("life-span");
 const button = document.getElementById("button");
 
 // Con promesas
 function getDogs() {
-  fetch(URL)
+  fetch(API_URL)
     .then((res) => res.json())
     .then((data) => {
-      img.src = data[0].url;
-      if (data[0].breeds[0]) {
-        title.innerText = data[0].breeds[0].name;
-        weight.innerText = "Peso: " + data[0].breeds[0].weight.metric + " kg";
-        height.innerText = "Altura: " + data[0].breeds[0].height.metric + " cm";
-        lifeSpan.innerText =
-          "Esperanza de vida: " + data[0].breeds[0].life_span;
-      } else {
-        title.innerText = "Sin información";
-        weight.innerText = "Peso: Sin información";
-        height.innerText = "Altura: Sin información";
-        lifeSpan.innerText = "Esperanza de vida: Sin información";
+      for (let i = 0; i < data.length; i++) {
+        images[i].src = data[i].url;
+        if (data[i].breeds[0]) {
+          titles[i].innerText = data[i].breeds[0].name;
+          weights[i].innerText =
+            "Peso: " + data[i].breeds[0].weight.metric + " kg";
+          heights[i].innerText =
+            "Altura: " + data[i].breeds[0].height.metric + " cm";
+          lifeSpan[i].innerText =
+            "Esperanza de vida: " + data[i].breeds[0].life_span;
+        } else {
+          titles[i].innerText = "Sin información";
+          weights[i].innerText = "Peso: Sin información";
+          heights[i].innerText = "Altura: Sin información";
+          lifeSpan[i].innerText = "Esperanza de vida: Sin información";
+        }
       }
     });
 }
 
 // Con async/await
 async function getDogs2() {
-  const res = await fetch(URL);
+  const res = await fetch(API_URL);
   const data = await res.json();
-  img.src = data[0].url;
-  if (data[0].breeds[0]) {
-    title.innerText = data[0].breeds[0].name;
-    weight.innerText = "Peso: " + data[0].breeds[0].weight.metric + " kg";
-    height.innerText = "Altura: " + data[0].breeds[0].height.metric + " cm";
-    lifeSpan.innerText = "Esperanza de vida: " + data[0].breeds[0].life_span;
-  } else {
-    title.innerText = "Sin información";
-    weight.innerText = "Peso: Sin información";
-    height.innerText = "Altura: Sin información";
-    lifeSpan.innerText = "Esperanza de vida: Sin información";
+  for (let i = 0; i < data.length; i++) {
+    images[i].src = data[i].url;
+    if (data[i].breeds[0]) {
+      titles[i].innerText = data[i].breeds[0].name;
+      weights[i].innerText = "Peso: " + data[i].breeds[0].weight.metric + " kg";
+      heights[i].innerText =
+        "Altura: " + data[i].breeds[0].height.metric + " cm";
+      lifeSpan[i].innerText =
+        "Esperanza de vida: " + data[i].breeds[0].life_span;
+    } else {
+      titles[i].innerText = "Sin información";
+      weights[i].innerText = "Peso: Sin información";
+      heights[i].innerText = "Altura: Sin información";
+      lifeSpan[i].innerText = "Esperanza de vida: Sin información";
+    }
   }
 }
 
